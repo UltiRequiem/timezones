aafb6cb5acdf0c1656f0b095dcfff0b778be49a2
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-02 13:35:44 -0500

    :wrench: Update entry points

--- scripts/node_support.ts
+++ scripts/node_support.ts
@@ -1,11 +1,19 @@
 import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.12/node_support.ts";
 
-buildPackage({
-  repoName: "timezones",
-  name: "@ultirequiem/timezones",
-  description: "All possible timezones and utils around it.",
-  homepage: "https://timezones.js.org",
-  keywords: ["timezones", "time", "date"],
-  license: "MIT",
-  version: "1.2.0",
-});
+buildPackage(
+  {
+    repoName: "timezones",
+    name: "@ultirequiem/timezones",
+    description: "All possible timezones and utils around it.",
+    homepage: "https://timezones.js.org",
+    keywords: ["timezones", "time", "date"],
+    license: "MIT",
+    version: "1.2.0",
+  },
+  {
+    entryPoints: [
+      "./mod.ts",
+      { name: "timezones", path: "./timezones.ts", kind: "bin" },
+    ],
+  }
+);
