65c8b14cd8f00c2270daab4efd4474ade5977310
GitHub <noreply@github.com>
2022-12-01 00:12:47 +0000

    ⬆️  Update dependencies

--- timezones.ts
+++ timezones.ts
@@ -4,7 +4,7 @@ import {
   bgBlue,
   bgGreen,
   black,
-} from "https://deno.land/std@0.161.0/fmt/colors.ts";
+} from "https://deno.land/std@0.166.0/fmt/colors.ts";
 
 import { randomTimeZone, type TimeZone, timeZones } from "./mod.ts";
 
