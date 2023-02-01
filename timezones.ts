be36973c4744e23b594d60a3cf2547491ff2daf1
GitHub <noreply@github.com>
2023-02-01 00:12:01 +0000

    ⬆️  Update dependencies

--- timezones.ts
+++ timezones.ts
@@ -1,10 +1,10 @@
 import Fuse from "https://esm.sh/fuse.js@6.6.2/";
-import { Command } from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
+import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
 import {
   bgBlue,
   bgGreen,
   black,
-} from "https://deno.land/std@0.166.0/fmt/colors.ts";
+} from "https://deno.land/std@0.175.0/fmt/colors.ts";
 
 import { randomTimeZone, type TimeZone, timeZones } from "./mod.ts";
 
